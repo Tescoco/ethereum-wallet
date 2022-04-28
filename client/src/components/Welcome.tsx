0bd4e777a6ea07a594c6a3ce702fc78fec96c456
Teslim Akinremi <ajokeakinremi43@gmail.com>
2022-04-29 00:04:31 +0100

    smart contract half installed

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -36,12 +36,12 @@ function Welcome() {
 
   return (
     <div className="flex w-fill justify-center items-center">
-      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+      <div className="flex mf:flex-row flex-col items-start justify-between mf:p-20 py-12 px-4">
         <div className="flex flex-1 justify-start flex-col md:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
             Send Crypto <br /> across the world
           </h1>
-          <p className="text-white text-left mt-5 font-light md:w-9/12 w-11">
+          <p className="text-white text-left mt-5 font-light md:w-9/12 w-11/12 text-base">
             Explore the crypto world. Buy and sell cryptocurrencies easily on
             krypt
           </p>
@@ -61,7 +61,7 @@ function Welcome() {
             <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
           </div>
         </div>
-        <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+        <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
           <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
             <div className="flex justify-between flex-col w-full h-full">
               <div className="flex justify-between items-start">
