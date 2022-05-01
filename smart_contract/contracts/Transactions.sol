f1d3d6ed405fb7f9cd05e833c35f90fc473d5de1
Teslim Akinremi <ajokeakinremi43@gmail.com>
2022-05-01 20:15:52 +0100

    client update

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -1,7 +1,9 @@
+//SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.0;
 
 contract Transactions {
-    uint256 transactionsCounter;
+    uint256 transactionCount;
 
     event Transfer(
         address from,
@@ -23,9 +25,43 @@ contract Transactions {
 
     TransferStruct[] transactions;
 
-    function addToBlockChain() public {}
+    function addToBlockChain(
+        address payable receiver,
+        uint256 amount,
+        string memory message,
+        string memory keyword
+    ) public {
+        transactionCount += 1;
+        transactions.push(
+            TransferStruct(
+                msg.sender,
+                receiver,
+                amount,
+                message,
+                block.timestamp,
+                keyword
+            )
+        );
 
-    function addToBlockChain() public {}
+        emit Transfer(
+            msg.sender,
+            receiver,
+            amount,
+            message,
+            block.timestamp,
+            keyword
+        );
+    }
 
-    function addToBlockChain() public {}
+    function getAllTransactions()
+        public
+        view
+        returns (TransferStruct[] memory)
+    {
+        return transactions;
+    }
+
+    function getTransactionCount() public view returns (uint256) {
+        return transactionCount;
+    }
 }
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,4 +1,16 @@
-//https://eth-ropsten.alchemyapi.io/v2/NXJh9cmXQoUCT2680tQlaxPP9mIaOoqQ
+//
 
 require("@nomiclabs/hardhat-waffle")
+
+module.exports =
+{
+  solidity: "0.8.0",
+  network: {
+    ropsten: {
+      url: "https://eth-ropsten.alchemyapi.io/v2/NXJh9cmXQoUCT2680tQlaxPP9mIaOoqQ",
+      accounts: ['7e38ded4e04244824d8398f611b2a24a2afd54bbf3dce75f9ad3c7a717bd4cdd']
+    }
+  }
+}
+
 
